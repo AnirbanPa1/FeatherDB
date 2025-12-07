93aca479b339f7da25439c83edbb497cbe7ce3f1
AnirbanPa1 <palanirban2003@gmail.com>
2025-12-07 20:45:55 +0530

    Part 3 - An In-Memory, Append-Only, Single-Table Database

--- src/db.c
+++ src/db.c
@@ -6,15 +6,20 @@
 
 void print_prompt() { printf("FeatherDB > "); }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+  Table *table = new_table();
   InputBuffer *input_buffer = new_input_buffer();
   print_logo();
-  while (true) {
+  while (true)
+  {
     print_prompt();
     read_input(input_buffer);
 
-    if (input_buffer->buffer[0] == '.') {
-      switch (do_meta_command(input_buffer)) {
+    if (input_buffer->buffer[0] == '.')
+    {
+      switch (do_meta_command(input_buffer))
+      {
       case (META_COMMAND_SUCCESS):
         continue;
       case (META_COMMAND_UNRECOGNIZED_COMMAND):
@@ -24,16 +29,26 @@ int main(int argc, char **argv) {
     }
 
     Statement statement;
-    switch (prepare_statement(input_buffer, &statement)) {
+    switch (prepare_statement(input_buffer, &statement))
+    {
     case (PREPARE_SUCCESS):
       break;
+    case (PREPARE_SYNTAX_ERROR):
+      printf("Syntax error. Could not parse statement.\n");
+      continue;
     case (PREPARE_UNRECOGNIZED_STATEMENT):
-      printf("Unrecognized keyword at the start of '%s'.\n",
-             input_buffer->buffer);
+      printf("Unrecognized keyword at the start of '%s'.\n", input_buffer->buffer);
       continue;
     }
 
-    execute_statement(&statement);
-    printf("Executed.\n");
+    switch (execute_statement(&statement, table))
+    {
+    case (EXECUTE_SUCCESS):
+      printf("Executed.\n");
+      break;
+    case (EXECUTE_TABLE_FULL):
+      printf("Error: Table full.\n");
+      break;
+    }
   }
 }
--- src/dbutils.h
+++ src/dbutils.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+#include "table.h"
+
 typedef enum
 {
     META_COMMAND_SUCCESS,
@@ -13,6 +15,7 @@ typedef enum
 typedef enum
 {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -22,9 +25,16 @@ typedef enum
     STATEMENT_SELECT
 } StatementType;
 
+typedef enum
+{
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
+
 typedef struct
 {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
 typedef struct
@@ -81,6 +91,14 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
         statement->type = STATEMENT_INSERT;
+        int arg_assigned = sscanf(
+            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+            statement->row_to_insert.username, statement->row_to_insert.email);
+
+        if (arg_assigned < 3)
+        {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0)
@@ -92,15 +110,38 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(Statement *statement)
+ExecuteResult execute_insert(Statement *statement, Table *table)
+{
+    if (table->num_rows >= TABLE_MAX_ROWS)
+    {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement *statement, Table *table)
+{
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++)
+    {
+        deserialize_row(row_slot(table, i), &row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement *statement, Table *table)
 {
     switch (statement->type)
     {
     case (STATEMENT_INSERT):
-        printf("This is Where we would do an insert.\n");
-        break;
+        return execute_insert(statement, table);
     case (STATEMENT_SELECT):
-        printf("This is Where we would do a select.\n");
-        break;
+        return execute_select(statement, table);
     }
 }
