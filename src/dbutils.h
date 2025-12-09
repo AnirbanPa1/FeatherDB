23617b3f66054161d3dd07d14ba68102a2dacc81
AnirbanPa1 <palanirban2003@gmail.com>
2025-12-09 16:07:55 +0530

    updated src/

--- src/dbutils.h
+++ src/dbutils.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #include "table.h"
 
@@ -16,6 +17,8 @@ typedef enum
 {
     PREPARE_SUCCESS,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIVE_ID,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -86,20 +89,46 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer)
     }
 }
 
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement)
+{
+    statement->type = STATEMENT_INSERT;
+
+    char *keyword = strtok(input_buffer->buffer, " ");
+    char *id_string = strtok(NULL, " ");
+    char *username = strtok(NULL, " ");
+    char *email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL)
+    {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if (id < 0)
+    {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
-        statement->type = STATEMENT_INSERT;
-        int arg_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email);
-
-        if (arg_assigned < 3)
-        {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select") == 0)
     {
@@ -131,6 +160,7 @@ ExecuteResult execute_select(Statement *statement, Table *table)
     for (uint32_t i = 0; i < table->num_rows; i++)
     {
         deserialize_row(row_slot(table, i), &row);
+        printf("(%d, %s, %s)\n", row.id, row.username, row.email);
     }
     return EXECUTE_SUCCESS;
 }
--- src/table.h
+++ src/table.h
@@ -1,26 +1,28 @@
-#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdint.h>
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
-
-typedef struct
-{
-    uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
-} Row;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
 
 #define ID_SIZE sizeof(((Row *)0)->id)
 #define USERNAME_SIZE sizeof(((Row *)0)->username)
 #define EMAIL_SIZE sizeof(((Row *)0)->email)
+
 #define ID_OFFSET 0
 #define USERNAME_OFFSET (ID_OFFSET + ID_SIZE)
 #define EMAIL_OFFSET (USERNAME_OFFSET + USERNAME_SIZE)
 #define ROW_SIZE (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE)
+
+#define PAGE_SIZE 4096
+#define TABLE_MAX_PAGES 100
+#define ROWS_PER_PAGE (PAGE_SIZE / ROW_SIZE)
+#define TABLE_MAX_ROWS (ROWS_PER_PAGE * TABLE_MAX_PAGES)
 
 // static const uint32_t ID_SIZE = size_of_attribute(Row, id);
 // static const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
@@ -29,6 +31,24 @@ typedef struct
 // static const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 // static const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 // static const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// const uint32_t PAGE_SIZE = 4096;
+// #define TABLE_MAX_PAGES 100
+// const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+// const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct
+{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
+} Row;
+
+typedef struct
+{
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+} Table;
 
 void serialize_row(Row *source, void *destination)
 {
@@ -43,22 +63,6 @@ void deserialize_row(void *source, Row *destination)
     memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
-
-#define PAGE_SIZE 4096
-#define TABLE_MAX_PAGES 100
-#define ROWS_PER_PAGE PAGE_SIZE / ROW_SIZE
-#define TABLE_MAX_ROWS ROWS_PER_PAGE *TABLE_MAX_PAGES
-
-// const uint32_t PAGE_SIZE = 4096;
-// #define TABLE_MAX_PAGES 100
-// const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-// const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
-
-typedef struct
-{
-    uint32_t num_rows;
-    void *pages[TABLE_MAX_PAGES];
-} Table;
 
 void *row_slot(Table *table, uint32_t row_num)
 {
